97c4b8ab2524aa843154fbebbcc43b35f93fe1fd
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-22 12:07:38 -0300

    feat: add new functions to void array

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,9 +6,11 @@
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 02:57:07 by antthoma          #+#    #+#             */
-/*   Updated: 2023/02/22 02:38:39 by antthoma         ###   ########.fr       */
+/*   Updated: 2023/02/22 15:34:49 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+# include <stdio.h>
 
 # include <unistd.h>
 # include "libft.h"
@@ -24,6 +26,7 @@ int		last_p_array(int *array);
 void	push(int *array, int value);
 void	push_void(int total, void **array, int value);
 int		pop(int *array);
+int		pop_void(int total, void **array);
 int		peek(int *array);
 void	ft_sort(int total, int *array);
 void	ft_sort_void(int total, void **array);
@@ -31,4 +34,4 @@ int		sort_s(int *array, int *swap, char c);
 int		sort_r(int *array, int *swap, char c);
 int		sort_rr(int *array, int *swap, char c);
 int		sort_p(int *array, int *swap, char c);
-int		sort_p_void(void **array, void **swap, char c);
+int		sort_p_void(int total, void **array, void **swap, char c);

aa7c4e50d013034de68f9e68d69723090acbbe91
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-25 10:06:31 -0300

    feat: change last_array

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 02:57:07 by antthoma          #+#    #+#             */
-/*   Updated: 2023/02/25 03:31:34 by antthoma         ###   ########.fr       */
+/*   Updated: 2023/02/25 05:34:56 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ int		push_swap_needed(int total, void **array);
 void	*new_array_void(int total);
 void	set_clean_void(int total, void **array);
 void	print_array_void(int total, void **array);
-int		last_p_array(int *array);
+int		last_p_array_void(int total, void **array);
 void	set_erease_void(int total, void **array);
 void	push_void(int total, void **array, int value);
 int		pop_void(int total, void **array);
 void	ft_sort(int total, int *array);
 void	ft_sort_void(int total, void **array);
-int		sort_s(int *array, int *swap, char c);
-int		sort_r(int *array, int *swap, char c);
-int		sort_rr(int *array, int *swap, char c);
+int		sort_s_void(int total, void **array, void **swap, char c);
+int		sort_r_void(int total, void **array, void **swap, char c);
+int		sort_rr_void(int total, void **array, void **swap, char c);
 int		sort_p_void(int total, void **array, void **swap, char c);
 #endif

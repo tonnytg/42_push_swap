d584f6bb4c0e3c0b872d520016b66de99fafbcb0
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-24 22:19:43 -0300

    feat: add new functions
     * set_erease_void

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,32 +6,31 @@
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 02:57:07 by antthoma          #+#    #+#             */
-/*   Updated: 2023/02/22 15:34:49 by antthoma         ###   ########.fr       */
+/*   Updated: 2023/02/25 02:17:35 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <stdio.h>
+#ifndef LIBFT_H
+# define LIBFT_H
 
+# include <stdlib.h>
 # include <unistd.h>
 # include "libft.h"
 
 int		check_values(int total, char **values);
 void	push_swap(int argc, char **argv);
-int		*new_array(int total);
+int		push_swap_needed(int total, void **array);
 void	*new_array_void(int total);
-void	set_clean(int total, int *array);
-void	print_array(int total, int *array);
+void	set_clean_void(int total, void **array);
 void	print_array_void(int total, void **array);
 int		last_p_array(int *array);
-void	push(int *array, int value);
+void	set_erease_void(int total, void **array);
 void	push_void(int total, void **array, int value);
-int		pop(int *array);
 int		pop_void(int total, void **array);
-int		peek(int *array);
 void	ft_sort(int total, int *array);
 void	ft_sort_void(int total, void **array);
 int		sort_s(int *array, int *swap, char c);
 int		sort_r(int *array, int *swap, char c);
 int		sort_rr(int *array, int *swap, char c);
-int		sort_p(int *array, int *swap, char c);
 int		sort_p_void(int total, void **array, void **swap, char c);
+#endif

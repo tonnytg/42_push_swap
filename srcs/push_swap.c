277fd8766fb030e90a7d24ea1d112c276ae5f1c3
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-02-24 22:21:41 -0300

    feat: review malloc code

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -1,35 +1,80 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   push_swap_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/18 02:55:02 by antthoma          #+#    #+#             */
-/*   Updated: 2023/02/25 02:14:51 by antthoma         ###   ########.fr       */
+/*   Created: 2023/02/21 13:20:42 by antthoma          #+#    #+#             */
+/*   Updated: 2023/02/25 02:08:43 by antthoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_swap(int argc, char **argv)
+int	push_swap_needed(int total, void **array)
 {
-	void	**array;
-	int		i;
-	int		total;
+	int	i;
 
-	total = argc - 1;
-	array = new_array_void(total);
-	i = total;
-	while (1 <= i)
+	i = 0;
+	while (i < total - 1)
 	{
-		push_void(total, array, ft_atoi(argv[i]));
-		i--;
+		if (array[i] != NULL && array[i + 1] != NULL)
+		{
+			if (*(int *)array[i] < *(int *)array[i + 1])
+				return (1);
+		}
+		i++;
 	}
-	if (push_swap_needed(total, array))
+	return (0);
+}
+
+void push_void(int total, void **array, int value)
+{
+	int *value_ptr;
+	int i;
+
+	i = 0;
+	while (array[i] != NULL)
 	{
-		ft_sort_void(total, array);	
+		i++;
 	}
-	set_erease_void(total, array);
-	free(array);
+	if (i == total)
+	{
+		// TODO: Validar se no PDF pode imprimir erro em algum momento
+		ft_printf("Panic, array it is full");
+		exit (1);
+	}
+	value_ptr = malloc(sizeof(int));
+	*value_ptr = value;
+	array[i] = value_ptr;
+}
+
+int	pop_void(int total, void **array)
+{
+	int	i;
+	int	top;
+
+	i = 0;
+	while (array[i] != NULL && i < total)
+	{
+		i++;
+	}
+	top = *(int *)array[i - 1];
+	array[i - 1] = NULL;
+	return (top);
+}
+
+int	peek_void(int total, void **array)
+{
+	int	i;
+	int	top;
+
+	i = 0;
+	while (array[i] != NULL && i < total)
+	{
+		i++;
+	}
+	top = *(int *)array[i - 1];
+	return (top);
 }

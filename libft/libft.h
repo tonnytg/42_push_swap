20a0942704972153e73c1d3a6f48e98f37e213f4
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-03-04 23:14:11 -0300

    Feat: add new signature for strcmp

--- libft/libft.h
+++ libft/libft.h
@@ -50,6 +50,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
+int	ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);

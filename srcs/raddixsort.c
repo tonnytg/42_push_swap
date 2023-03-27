682dde3fe1cdaa36d3e04facccb61116b4134599
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-03-26 22:49:19 -0300

    feat: add logic for bits

--- srcs/raddixsort.c
+++ srcs/raddixsort.c
@@ -12,34 +12,65 @@
 
 #include "push_swap.h"
 
+int	get_max_indice(int total, void **array)
+{
+	int	i;
+	int	max;
+	int *value_ptr;
+
+	i = 0;
+	max = 0;
+	while (i < total)
+	{
+		value_ptr = array[i];
+		if (value_ptr[1] > max)
+			max = value_ptr[1];
+		i++;
+	}
+	return (max);
+}
+
+int	get_count_bit(int value)
+{
+	int	i;
+	int count;
+	char *temp;
+
+	i = 8;
+	count = 0;
+	temp = ft_char_to_bit(value);
+	while (i > 0)
+	{
+		if (temp[i] == '1')
+			count = i;
+		i--;
+	}
+	count = 8 - count;
+	return (count);
+}
+
 void	raddixsort(int total, void **array)
 {
 	void	**swap;
 	int i;
 	int *value_ptr;
-	int *value_ptr2;
-	char *mask;
+	int max;
+	int counter;
 
 	swap = new_array_void(total);
 
-	// create raddixsort to sort array of void using the indice of vector, each indice need to convert to bit
-	// this can be done using a mask, and then shift the mask to the right
-	// the mask will be 1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000
+	max = get_max_indice(total, array);
+	counter = 0;
+	counter = get_count_bit(max);
+	ft_printf("counter: %d - max: %d\n", counter, max);
+
 	i = 0;
 	while (i < total)
 	{
 		value_ptr = array[i];
-		ft_printf("value_ptr: %d", *value_ptr);
-		// char    *ft_char_to_bit(int dec)
-		mask = ft_char_to_bit(value_ptr[1]);
-		value_ptr = (int *)array[i];
-		if (value_ptr[1] == 0)
-			push_void(total, swap, array[i]);
-		else
-			push_void(total, array, array[i]);
+		ft_printf("%s\n", ft_char_to_bit(value_ptr[1]));
 		i++;
 	}
-
 
 	print_indices(total, array);
 	free(swap);

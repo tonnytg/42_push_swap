b0a3f44c837dd1cc617dbc499321dc798e379ff4
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-03-27 20:03:18 -0300

    feat: test with raddix sort

--- srcs/raddixsort.c
+++ srcs/raddixsort.c
@@ -49,27 +49,93 @@ int	get_count_bit(int value)
 	return (count);
 }
 
-void	raddixsort(int total, void **array)
+//void	raddixsort(int total, void **array)
+//{
+//	void	**swap;
+//	int i;
+//	int *value_ptr;
+//	int max;
+//	int counter;
+//
+//	swap = new_array_void(total);
+//
+//	max = get_max_indice(total, array);
+//	counter = 0;
+//	counter = get_count_bit(max);
+//	ft_printf("counter: %d - max: %d\n", counter, max);
+//
+//	// create count sort with bit
+//	// TODO: create a function to use Counting Sort to organize bits
+//
+//
+//	print_indices(total, array);
+//	free(swap);
+//}
+
+int get_bit(int value, int bit)
 {
-	void	**swap;
+	return (value >> bit) & 1;
+}
+
+void counting_sort_bit(int total, void **array, void **swap, int bit)
+{
 	int i;
+	int count[2] = {0, 0};
 	int *value_ptr;
-	int max;
-	int counter;
+	int index;
 
-	swap = new_array_void(total);
-
-	max = get_max_indice(total, array);
-	counter = 0;
-	counter = get_count_bit(max);
-	ft_printf("counter: %d - max: %d\n", counter, max);
-
+	// Contagem dos bits 0 e 1
 	i = 0;
 	while (i < total)
 	{
 		value_ptr = array[i];
-		ft_printf("%s\n", ft_char_to_bit(value_ptr[1]));
+		count[get_bit(value_ptr[1], bit)]++;
 		i++;
+	}
+	ft_printf("contagem dos bits1:%d\n", count[0]);
+	ft_printf("contagem dos bits2:%d\n", count[1]);
+
+	// Acumula os valores na contagem
+	count[1] += count[0];
+
+	// Aplica o Counting Sort baseado nos bits
+	i = total - 1;
+	while ( i >= 0)
+	{
+		value_ptr = array[i];
+		index = get_bit(value_ptr[1], bit);
+		count[index]--;
+		swap[count[index]] = array[i];
+		i--;
+	}
+
+	// Copia os elementos do array swap de volta para o array original
+	i = 0;
+	while (i < total)
+	{
+		array[i] = swap[i];
+		i++;
+	}
+}
+
+void raddixsort(int total, void **array)
+{
+	void **swap;
+	int max;
+	int num_bits;
+	int bit;
+
+	swap = new_array_void(total);
+
+	max = get_max_indice(total, array);
+	num_bits = get_count_bit(max);
+
+	// Aplica o Counting Sort em cada bit
+	bit = 0;
+	while (bit < num_bits)
+	{
+		counting_sort_bit(total, array, swap, bit);
+		bit++;
 	}
 
 	print_indices(total, array);

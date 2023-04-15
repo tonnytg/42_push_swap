9b5a9020573d051e17420822e897198bd5af72bd
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-14 22:06:12 -0300

    feat: add new sort for 3 elements

--- srcs/ft_sort.c
+++ srcs/ft_sort.c
@@ -12,33 +12,35 @@
 
 #include "push_swap.h"
 
-void ft_sort_two_void(int total, void **array)
-{
-		sort_s_void(total, array, NULL, 'a');
-}
-
 void ft_sort_three_void(int total, void **array)
 {
-	int	*value_ptr1;
-	int	*value_ptr2;
-	int	*value_ptr3;
+	int *value_ptr1;
+	int *value_ptr2;
+	int *value_ptr3;
+	int i;
 
-	value_ptr1 = array[0];
-	value_ptr2 = array[1];
-	value_ptr3 = array[2];
-	if (value_ptr1[0] > value_ptr2[0])
-		sort_s_void(total, array, NULL, 'a');
-	else if (value_ptr1[0] > value_ptr3[0])
-	{
-		sort_rr_void(total, array, NULL, 'a');
+	i = 0;
+	while (i < 2) {
+		value_ptr1 = array[0];
+		value_ptr2 = array[1];
+		value_ptr3 = array[2];
+
+		if (value_ptr3[1] > value_ptr1[1])
+			sort_r_void(total, array, NULL, 'a');
+		else if (value_ptr3[1] > value_ptr2[1])
+			sort_s_void(total, array, NULL, 'a');
+		i++;
 	}
+
+
+
 }
 
 void	ft_sort_void(int total, void **array)
 {
 	if (total == 2)
 	{
-		ft_sort_two_void(total, array);
+		sort_s_void(total, array, NULL, 'a');
 		return ;
 	}
 	if (total == 3)

518c0252db1883c5e95f0c6dc135a1ff221786ee
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-16 13:33:34 -0300

    fix: add null in array for peek

--- tests/stack_utils_test.c
+++ tests/stack_utils_test.c
@@ -53,6 +53,10 @@ Test(push_void, passing) {
 Test(peek_void, passing) {
 
 	void **stack = malloc(sizeof(void *) * 4);
+	stack[0] = NULL;
+	stack[1] = NULL;
+	stack[2] = NULL;
+	stack[3] = NULL;
 
 	int *value_ptr = malloc(sizeof(int) * 2);
 	value_ptr[0] = 0;
@@ -64,8 +68,9 @@ Test(peek_void, passing) {
 
 	stack[0] = value_ptr;
 	stack[1] = value_ptr1;
-	push_void(3, stack, 2, 2);
+	push_void(4, stack, 2, 2);
 
-	int *value = peek_void(3, stack);
+	int *value = peek_void(4, stack);
+	printf("value[0] = %d\n", value[0]);
 	cr_assert_eq(value[0], 2);
 }

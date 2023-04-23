1f2802404eb8b0a3c13be21b56afc584d2266873
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-04-22 21:30:45 -0300

    feat: solve problem with 3 1 2

---
+++ srcs/sort_three_elements.c
@@ -0,0 +1,18 @@
+#include "../includes/push_swap.h"
+
+void	ft_sort_three_elements(int total, t_pair *array)
+{
+	int			i;
+
+	i = 0;
+	while (i < total)
+	{
+		if (array[2].index > array[0].index)
+			sort_r_void(total, array, NULL, 'a');
+		if (array[2].index > array[1].index)
+			sort_s_void(total, array, NULL, 'a');
+		if (array[2].index > array[0].index )
+			sort_rr_void(total, array, NULL, 'a');
+		i++;
+	}
+}
